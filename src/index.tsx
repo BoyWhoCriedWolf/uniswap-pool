05c14bc1a8c256784c01bb245c66f39831e3e60a
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-07-05 23:35:58 +0800

    fix: eslit prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,40 @@
-import '@reach/dialog/styles.css'
-import 'inter-ui'
-import 'polyfills'
-import 'tracing'
-import 'connection/eagerlyConnect'
+import "@reach/dialog/styles.css";
+import "inter-ui";
+import "polyfills";
+import "tracing";
+import "connection/eagerlyConnect";
 
-import { ApolloProvider } from '@apollo/client'
-import { FeatureFlagsProvider } from 'featureFlags'
-import { apolloClient } from 'graphql/data/apollo'
-import { BlockNumberProvider } from 'lib/hooks/useBlockNumber'
-import { MulticallUpdater } from 'lib/state/multicall'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Provider } from 'react-redux'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
-import { SystemThemeUpdater, ThemeColorMetaUpdater } from 'theme/components/ThemeToggle'
-import { isBrowserRouterEnabled } from 'utils/env'
+import { ApolloProvider } from "@apollo/client";
+import { FeatureFlagsProvider } from "featureFlags";
+import { apolloClient } from "graphql/data/apollo";
+import { BlockNumberProvider } from "lib/hooks/useBlockNumber";
+import { MulticallUpdater } from "lib/state/multicall";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import {
+  SystemThemeUpdater,
+  ThemeColorMetaUpdater,
+} from "theme/components/ThemeToggle";
+import { isBrowserRouterEnabled } from "utils/env";
 
-import Web3Provider from './components/Web3Provider'
-import { LanguageProvider } from './i18n'
-import App from './pages/App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import store from './state'
-import ApplicationUpdater from './state/application/updater'
-import ListsUpdater from './state/lists/updater'
-import LogsUpdater from './state/logs/updater'
-import OrderUpdater from './state/signatures/updater'
-import TransactionUpdater from './state/transactions/updater'
-import ThemeProvider, { ThemedGlobalStyle } from './theme'
-import RadialGradientByChainUpdater from './theme/components/RadialGradientByChainUpdater'
+import Web3Provider from "./components/Web3Provider";
+import { LanguageProvider } from "./i18n";
+import App from "./pages/App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import store from "./state";
+import ApplicationUpdater from "./state/application/updater";
+import ListsUpdater from "./state/lists/updater";
+import LogsUpdater from "./state/logs/updater";
+import OrderUpdater from "./state/signatures/updater";
+import TransactionUpdater from "./state/transactions/updater";
+import ThemeProvider, { ThemedGlobalStyle } from "./theme";
+import RadialGradientByChainUpdater from "./theme/components/RadialGradientByChainUpdater";
 
 if (window.ethereum) {
-  window.ethereum.autoRefreshOnNetworkChange = false
+  window.ethereum.autoRefreshOnNetworkChange = false;
 }
 
 function Updaters() {
@@ -47,14 +50,14 @@ function Updaters() {
       <MulticallUpdater />
       <LogsUpdater />
     </>
-  )
+  );
 }
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-const container = document.getElementById('root') as HTMLElement
+const container = document.getElementById("root") as HTMLElement;
 
-const Router = isBrowserRouterEnabled() ? BrowserRouter : HashRouter
+const Router = isBrowserRouterEnabled() ? BrowserRouter : HashRouter;
 
 createRoot(container).render(
   <StrictMode>
@@ -80,8 +83,8 @@ createRoot(container).render(
       </FeatureFlagsProvider>
     </Provider>
   </StrictMode>
-)
+);
 
-if (process.env.REACT_APP_SERVICE_WORKER !== 'false') {
-  serviceWorkerRegistration.register()
+if (process.env.REACT_APP_SERVICE_WORKER !== "false") {
+  serviceWorkerRegistration.register();
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,23 +1,23 @@
-import 'polyfill-object.fromentries'
+import "polyfill-object.fromentries";
 
-import { ResizeObserver } from '@juggle/resize-observer'
-import flat from 'array.prototype.flat'
-import flatMap from 'array.prototype.flatmap'
-import { Buffer } from 'buffer'
+import { ResizeObserver } from "@juggle/resize-observer";
+import flat from "array.prototype.flat";
+import flatMap from "array.prototype.flatmap";
+import { Buffer } from "buffer";
 
-flat.shim()
-flatMap.shim()
+flat.shim();
+flatMap.shim();
 
 declare global {
   interface Window {
-    Buffer: typeof Buffer
+    Buffer: typeof Buffer;
   }
 }
 
 if (!window.Buffer) {
-  window.Buffer = Buffer
+  window.Buffer = Buffer;
 }
 
 if (!window.ResizeObserver) {
-  window.ResizeObserver = ResizeObserver
+  window.ResizeObserver = ResizeObserver;
 }

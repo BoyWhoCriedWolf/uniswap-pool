e37492b2d75c9365e3e9a4af4722f4cc0b992061
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-07-07 14:42:24 +0800

    fix: remove dev flag box

--- src/components/TopLevelModals/index.tsx
+++ src/components/TopLevelModals/index.tsx
@@ -7,7 +7,6 @@ import AddressClaimModal from "components/claim/AddressClaimModal";
 import ConnectedAccountBlocked from "components/ConnectedAccountBlocked";
 import FiatOnrampModal from "components/FiatOnrampModal";
 import { UkDisclaimerModal } from "components/NavBar/UkDisclaimerModal";
-import DevFlagsBox from "dev/DevFlagsBox";
 import useAccountRiskCheck from "hooks/useAccountRiskCheck";
 import Bag from "nft/components/bag/Bag";
 import TransactionCompleteModal from "nft/components/collection/TransactionCompleteModal";
@@ -41,7 +40,7 @@ export default function TopLevelModals() {
       <AirdropModal />
       <FiatOnrampModal />
       <UkDisclaimerModal />
-      {shouldShowDevFlags && <DevFlagsBox />}
+      {/* {shouldShowDevFlags && <DevFlagsBox />} */}
     </>
   );
 }
